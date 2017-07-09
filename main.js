a2ae4ed04bac0a04112ea3aa66ac8b2baf581d8e
jjaryupwork <jjaryupwork@gmail.com>
2017-07-10 00:04:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,15 @@
-$('#start').click(function(){
-	$('.raff').animate({
-		marginLeft: "+=800px",
-	}, 6000);
-	$('.raff').animate({
-		marginLeft: "-=800px",
-	}, 6000);
-	$('.rocket').animate({
-		top: "-=950px",
-	}, 16000);
+$('.close').click(function() {
+    $('.cookiesheader').hide();
+});
 
-
-	/*$('.Container').animate({
-		marginLeft: "+=1350px",
-	}, 6000);
-	$('.Container').animate({
-		marginLeft: "-=1350px",
-	}, 6000);
-	$('.Container').animate({
-		marginTop: "+=650px",
-	}, 6000);
-	$('.Container').animate({
-		marginLeft: "+=1350px",
-	}, 6000);
-	$('.Container').animate({
-		marginLeft: "-=1350px",
-	}, 6000);
-	$('.Container').animate({
-		marginTop: "-=650px",
-	}, 6000);*/
-
-
-	});
+$('#start').click(function() {
+    $('.giraffe').animate({
+        marginLeft: "+=800px",
+    }, 6000);
+    $('.giraffe').animate({
+        marginLeft: "-=800px",
+    }, 6000);
+    $('.rocket').animate({
+        top: "-=950px",
+    }, 16000);
+});
